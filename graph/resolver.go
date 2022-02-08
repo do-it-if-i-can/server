a21d3dd118717f5c7c741a7a67e5aae56e91a3ea
s-kawabe <s.kawabe2281@gmail.com>
2022-02-08 09:53:26 +0900

    chore: fix environment

--- graph/resolver.go
+++ graph/resolver.go
@@ -4,7 +4,7 @@ import "github.com/do-it-if-i-can/server/graph/model"
 
 // This file will not be regenerated automatically.
 // このファイルは自動的に再生成されません
-//
+
 // It serves as dependency injection for your app, add any dependencies you require here.
 // アプリの依存性注入の役割を果たすもので、必要な依存性をここに追加します。
 
